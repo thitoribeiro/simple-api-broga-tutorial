4925e243ef7bfd14654e52a474dfdaed91fa9bf8
thitoribeiro <thito.ribeiro@hotmail.com>
2020-07-06 23:41:27 -0300

    POST method and body-parser install and config

--- app/index.js
+++ app/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  const result = { name: 'Thito', sobrenome: 'Ribeiro', };
+  const result = { name: 'Thito', surename: 'Ribeiro', };
   return res.json(result);
 });
+
+app.post('/', (req, res) => {
+  const name = req.body.name;
+  const surename = req.body.surename;
+  return res.json([name, surename]);
+});
+
+
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
