d0d24e245b9ef3e1748b3d867437a9d6354f65f0
thitoribeiro <thito.ribeiro@hotmail.com>
2020-07-08 23:32:39 -0300

    PUT method

--- app/index.js
+++ app/index.js
@@ -3,6 +3,13 @@ const bodyparser = require('body-parser');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const data = [
+  { id: 1, name: "Mike", age: 22, company: "Nascetur Mus Company" },
+  { id: 2, name: "Eleanor", age: 42, company: "Hendrerit Donec LLP" },
+  { id: 3, name: "Dylan", age: 51, company: "Nisi Incorporated" },
+  { id: 4, name: "Leila", age: 30, company: "Eros Non Limited" },
+  { id: 5, name: "Jason", age: 31, company: "Accumsan Interdum Associates" },
+];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,6 +22,11 @@ app.post('/', (req, res) => {
   const name = req.body.name;
   const surename = req.body.surename;
   return res.json([name, surename]);
+});
+
+app.put('/:identifier', (req,res) => {
+  const identifier = req.params.identifier;
+  return res.json([identifier]);
 });
 
 
